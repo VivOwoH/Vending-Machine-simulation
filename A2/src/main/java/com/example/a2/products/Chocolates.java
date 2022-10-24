36d7aaa2b26d333da3632630eae37531929692f4
VivianH <weha7612@uni.sydney.edu.au>
2022-10-24 15:36:30 +1100

    update product logic

--- A2/src/main/java/com/example/a2/products/Chocolates.java
+++ A2/src/main/java/com/example/a2/products/Chocolates.java
@@ -12,4 +12,8 @@ public class Chocolates extends Product {
     public String getCategoryStr() {
         return this.category;
     }
+
+    public void setCategoryStr(String category) {
+        this.category = category;
+    }
 }
